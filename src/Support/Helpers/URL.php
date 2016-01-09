9de3f778c428d23d63bf710e6868c81a2522b957
Casper Rasmussen <cr@nodes.dk>
2016-01-09 12:39:04 +0100

    updated shortcuts for url, removed a unused param and created a feature to resize image urls easy

--- src/Support/Helpers/URL.php
+++ src/Support/Helpers/URL.php
@@ -6,11 +6,52 @@ if (!function_exists('assets_get')) {
      * @author Casper Rasmussen <cr@nodes.dk>
      *
      * @param                                 $path
-     * @param \Nodes\Assets\Url\Settings|null $settings
      * @return string $url
      */
-    function assets_get($path, \Nodes\Assets\Url\Settings $settings = null)
+    function assets_get($path)
     {
-        return \Assets::get($path, $settings);
+        return \Assets::get($path);
+    }
+}
+
+if (!function_exists('assets_resize')) {
+
+    /**
+     * Append a query with h and w after the image url
+     *
+     * @author Casper Rasmussen <cr@nodes.dk>
+     *
+     * @param      $url
+     * @param bool $width
+     * @param bool $height
+     * @return string
+     */
+    function assets_resize($url, $width = false, $height = false)
+    {
+        // Validate the url
+        if(!filter_var($url, FILTER_VALIDATE_URL)) {
+            return $url;
+        }
+
+        // Init query
+        $query = [];
+
+        // Add w query param
+        if($width && is_numeric($width) && $width > 0) {
+            $query['w'] = (int) $width;
+        }
+
+        // Add h query param
+        if($height && is_numeric($height) && $height > 0) {
+            $query['w'] = (int) $height;
+        }
+
+        // Check if query is empty
+        if(empty($query)) {
+            return $url;
+        }
+
+        // Append the query string
+        return $url . '?' . http_build_query($query);
     }
 }
