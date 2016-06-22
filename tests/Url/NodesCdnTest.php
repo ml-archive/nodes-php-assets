a059f0f7b93137f82a0a3d76be54a0ed6c0e2ce0
Casper Rasmussen <cr@nodes.dk>
2016-06-22 11:46:02 +0000

    initial test setup

---
+++ tests/Url/NodesCdnTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Nodes\Assets\ServiceProvider;
+
+class RolesRepositoryTest extends Orchestra\Testbench\TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            ServiceProvider::class,
+        ];
+    }
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->assertTrue(true);
+    }
+}
